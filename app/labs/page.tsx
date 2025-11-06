cfe637ec866fb76fcb028122497d1a5428c7330b
Spenser_Sun <hsun112358@gmail.com>
2025-11-06 13:32:55 -0500

    fix julia mundy picture

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -100,7 +100,7 @@ const labs = [
     members: 16,
     description: "Something something molecular beam epitaxy.",
     researchArea: "Solid State",
-    image: "http://mundy.physics.harvard.edu/images/MBE.png",
+    image: "https://mundy.physics.harvard.edu/images/MBE.png",
     sampleProjects: [
       "Dark matter detection experiments",
       "CMB polarization analysis",
